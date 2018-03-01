45a93d4223579446e57c9936b5f1b0601d7d8183
WayoCarrasco <eduardo.carrasco.nz@gmail.com>
2018-03-01 12:38:15 -0300

    bodegas ondev

--- static/js/cart.config.js
+++ static/js/cart.config.js
@@ -60,11 +60,11 @@ $(document).ready(function()
     }
     var base_url = $.environmentVar(
         'https://apibodegas.loadingplay.com/',
-        'https://apibodegas.loadingplay.com/',
+        'https://apibodegas.ondev.today/',
         'https://apibodegas.loadingplay.com/');
     var checkout_url = $.environmentVar(
         'https://pay.loadingplay.com',
-        'https://pay.loadingplay.com',
+        'https://lpcheckout.ondev.today',
         'https://pay.loadingplay.com');
     var app_public = $.environmentVar(67,67,67);
     var site_name = $.environmentVar('tikashop', 'tikashop', 'tikashop');
